9b285b57396256ca6d20da636460569215c6f65f
The Nail Bar Dresden <thenailbar.dresden@gmail.com>
2023-07-27 18:28:12 +0200

    Changed the nail bar to the beauty bar

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -12,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'the-nail-bar-dresden'`, () => {
+  it(`should have as title 'the-beauty-bar-dresden'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('the-nail-bar-dresden');
+    expect(app.title).toEqual('the-beauty-bar-dresden');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('the-nail-bar-dresden app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('the-beauty-bar-dresden app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'the-nail-bar-dresden';
+  title = 'the-beauty-bar-dresden';
 }
